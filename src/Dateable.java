32dc82cc5fdcbaaa2b253fac0eb6b7562a39d066
tristanl2027 <tristanl2027@student.cis.edu.hk>
2024-08-26 08:54:12 +0800

    Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int datingAge = 7 + (age/2);
+        System.out.println(age+"-year olds should date somebody who is at least "+datingAge+" years old");
+        scanner.close();
+
     }
 }
