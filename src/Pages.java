d894a7bb8b1edb4b2f9427edd614fe2bf74b6cdd
tristanl2027 <tristanl2027@student.cis.edu.hk>
2024-08-26 08:54:29 +0800

    Pages.java

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int pages = 100-age;
+        System.out.println("A "+age+"-year old should read at least "+pages+" pages before giving up on a book.");
     }
 }
